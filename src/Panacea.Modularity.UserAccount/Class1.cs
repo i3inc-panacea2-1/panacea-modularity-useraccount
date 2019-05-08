62800431ca4307ace0eb5a7318c3b19dc74d0552
Portno <j.remoundos@dotbydot.gr>
2019-05-08 13:05:30 +0300

    first commit

---
+++ src/Panacea.Modularity.UserAccount/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.UserAccount
+{
+    public interface IUserAccountPlugin
+    {
+        void NavigateToRegister();
+
+        void NavigateToMyAccount();
+    }
+}
